7703331d310f6ad8e57e14b93e74dc27acdeaf03
Jack <62153707+JackCo100@users.noreply.github.com>
2025-08-06 21:43:43 +0100

    api setup

--- server/server.js
+++ server/server.js
@@ -2,14 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send({
-    origin: "London Kings Cross",
-    destination: "Edinburgh",
-    stops: ["Stevenage, Peterborough, York"],
-    departureTime: 1200
-  })
-})
+const axios = require('axios')
+const authKeys = require('./keys.js')
+console.log(authKeys)
+axios({
+  method: 'GET',
+  url: 'https://api.rtt.io/api/v1/json/search/BMH',
+  auth: {
+    username: authKeys.username,
+    password: authKeys.password
+  }
+}).then((res) => console.log(res.data))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
