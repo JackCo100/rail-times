3e58109497e3c20ce0b1469dd5df5b34a3700af3
Jack <62153707+JackCo100@users.noreply.github.com>
2025-08-14 23:17:43 +0100

    Very basic station departures api call

--- server/server.js
+++ server/server.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
-const axios = require('axios')
 const authKeys = require('./keys.js')
-console.log(authKeys)
-axios({
-  method: 'GET',
-  url: 'https://api.rtt.io/api/v1/json/search/BMH',
-  auth: {
-    username: authKeys.username,
-    password: authKeys.password
-  }
-}).then((res) => console.log(res.data))
+
+app.get('/stationBoard/:stnCode', async (req, res) => {
+  res.send(await fetch(`https://api.rtt.io/api/v1/json/search/` +req.params.stnCode , {
+    method: 'GET',
+    headers: {
+        'Authorization': `Basic ${btoa(authKeys.username + ":" + authKeys.password)}`,
+    },
+}).then((result) => result.json().then((resJson) => {return resJson}).catch(() => {return 'error getting station'})))
+})
+//TODO: better error handling
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
