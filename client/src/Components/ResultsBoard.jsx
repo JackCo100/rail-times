dce7f2da3d19f6197134f2e6cad905d42de3bf06
Jack <62153707+JackCo100@users.noreply.github.com>
2025-09-02 21:23:13 +0100

    css for results board

--- client/src/Components/ResultsBoard.jsx
+++ client/src/Components/ResultsBoard.jsx
@@ -1,12 +1,13 @@
+import styles from './ResultsBoard.module.css';
 export default function ResultsBoard({data}) {
     return (
     <div>
       <h1>Results</h1>
-      <p>{data.location.name}</p>
-      <table>
-        <thead>
+      <p>Departures for the next two hours from <b>{data.location.name}</b></p>
+      <table className="border border-slate-400 table-fixed w-full text-center">
+        <thead className="bg-slate-800 text-white">
             <tr>
-                <th>Scheduled Departure</th>
+                <th>Scheduled</th>
                 <th>Destination</th>
                 <th>Platform</th>
                 <th>Status</th>
@@ -15,10 +16,10 @@ export default function ResultsBoard({data}) {
         </thead>
         <tbody>
             {data.services.map((service, index) => (
-                <tr key={index}>
+                <tr key={index} className="odd:bg-white even:bg-gray-200">
                     <td>{service.locationDetail.gbttBookedDeparture}</td>
                     <td>{service.locationDetail.destination[0]['description']}</td>
-                    <td>{service.locationDetail.platform}</td>
+                    <td>{service.serviceType === "bus" ? "Bus Replacement" : service.locationDetail.platform}</td>
                     <td>{service.locationDetail.realtimeDeparture}</td>
                     <td>{service.atocName}</td>
                 </tr>
