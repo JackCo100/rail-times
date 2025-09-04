0b1133f4afb5649238a0e04564853387dfa8bbb0
Jack <62153707+JackCo100@users.noreply.github.com>
2025-09-04 21:06:52 +0100

    Setup service api call

--- server/server.js
+++ server/server.js
@@ -11,6 +11,15 @@ app.get('/getDepartures/:stnCode', async (req, res) => {
     },
 }).then((result) => result.json().then((resJson) => {return resJson}).catch(() => {return 'error getting station'})))
 })
+app.get('/getService/:Uid/:year/:month/:day', async (req, res) => {
+  console.log(`https://api.rtt.io/api/v1/json/search/` + req.params.Uid + '/' + req.params.year + '/' + req.params.month + '/' + req.params.day)
+  res.send(await fetch(`https://api.rtt.io/api/v1/json/service/` + req.params.Uid + '/' + req.params.year + '/' + req.params.month + '/' + req.params.day , {
+    method: 'GET',
+    headers: {
+        'Authorization': `Basic ${btoa(authKeys.username + ":" + authKeys.password)}`,
+    },
+}).then((result) => result.json().then((resJson) => {return resJson}).catch(() => {return 'error getting service'})))
+})
 //TODO: better error handling
 
 
