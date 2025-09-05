1d453666161d17eec55438cc246bcfb01dd986be
Jack <62153707+JackCo100@users.noreply.github.com>
2025-09-05 20:15:27 +0100

    modal trigger setup

--- client/src/Components/ResultsBoard.jsx
+++ client/src/Components/ResultsBoard.jsx
@@ -1,9 +1,14 @@
 import styles from './ResultsBoard.module.css';
-export default function ResultsBoard({data}) {
+export default function ResultsBoard({data, setServiceUid,setShowModal}) {
+    function handleClick(uid){
+        console.log(uid)
+        setServiceUid(uid)
+        setShowModal(true)
+    }
     return (
     <div>
       <h1>Results</h1>
-      <p>Departures for the next two hours from <b>{data.location.name}</b></p>
+      <p>Live departures from <b>{data.location.name}</b></p>
       <table className="border border-slate-400 table-fixed w-full text-center">
         <thead className="bg-slate-800 text-white">
             <tr>
@@ -15,8 +20,8 @@ export default function ResultsBoard({data}) {
             </tr>
         </thead>
         <tbody>
-            {data.services.map((service, index) => (
-                <tr key={index} className="odd:bg-white even:bg-gray-200">
+            {data.services.map((service) => (
+                <tr onClick={() => handleClick(service.serviceUid)} key={service.serviceUid} className="odd:bg-white even:bg-gray-200">
                     <td>{service.locationDetail.gbttBookedDeparture}</td>
                     <td>{service.locationDetail.destination[0]['description']}</td>
                     <td>{service.serviceType === "bus" ? "Bus Replacement" : service.locationDetail.platform}</td>
