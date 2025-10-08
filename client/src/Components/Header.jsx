8516a987526841c0e9cb7fb625ba8b9a3d024c9c
Jack <62153707+JackCo100@users.noreply.github.com>
2025-10-08 22:26:16 +0100

    header link to home

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -2,7 +2,7 @@ import styles from './Header.module.css';
 export default function NavBar() {
   return (
     <div>
-      <h1>UK Railway Times</h1>
+      <a className='text-2xl'  href='/'>UK Railway Times</a>
       <p>Enter a station code and find live arrivals and departures.</p>
     </div>
   );
