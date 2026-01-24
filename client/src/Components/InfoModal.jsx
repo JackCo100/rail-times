76af3431f7f3df35975be145c56f145b2a6f6212
Jack <62153707+JackCo100@users.noreply.github.com>
2026-01-24 19:57:28 +0000

    Default hide previous stations, add button to show previous stations

--- client/src/Components/InfoModal.jsx
+++ client/src/Components/InfoModal.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 export default function InfoModal( {serviceUid, stationCode, setShowModal, setServiceUid} ) {
     const [results, setResults] = useState(null);
     const [error, setError] = useState(false);
+    const [stationIndex, setStationIndex] = useState(null);
     const date = new Date()
     const formattedMonth = date.getMonth()+1 > 9 ? date.getMonth() + 1 : '0' + (date.getMonth() + 1) //add leading 0 to month for jan-sep as required by api call
     const formattedDay = date.getDate() > 9 ? date.getDate() : '0' + date.getDate()  //add leading 0 to day for 0-9 as required by api call
@@ -16,7 +17,9 @@ export default function InfoModal( {serviceUid, stationCode, setShowModal, setSe
         } else {
           setError(false);
           setResults(response.data)
+          setStationIndex(response.data.locations.findIndex(loc => loc.crs === stationCode))
           setShowModal(true)
+          
         }})
       .catch(error => { console.error('Error fetching data:', error)})
     }, [serviceUid])
@@ -25,6 +28,10 @@ export default function InfoModal( {serviceUid, stationCode, setShowModal, setSe
         setResults(null)
         setShowModal(false)
         setServiceUid(null)
+        setStationIndex(null)
+    }
+    function handleClickPrevious(){
+      setStationIndex(0)
     }
         
   return (
@@ -50,8 +57,10 @@ export default function InfoModal( {serviceUid, stationCode, setShowModal, setSe
             </tr>
             </thead>
             <tbody>
+              {stationIndex === 0 ? null : 
+              <tr className='border' onClick={handleClickPrevious}><td colSpan={3}>Show earlier stations</td></tr>}
               {
-              results.locations.map((loc) => (
+              results.locations.slice(stationIndex).map((loc) => (
                 <tr key={loc.crs} className={loc.crs === stationCode ? "bg-gray-200 border px-4 py-2" : "border px-4 py-2"}>
                     <td className="border px-4 py-2">{loc.description}</td>
                     <td className="border px-4 py-2">{loc.gbttBookedDeparture ? (loc.gbttBookedDeparture).slice(0,2) + ":" + (loc.gbttBookedDeparture).slice(2,4) : (loc.gbttBookedArrival).slice(0,2) + ":" + (loc.gbttBookedArrival).slice(2,4) }</td>
