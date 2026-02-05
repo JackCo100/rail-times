51f167c72d36e54128f3b35829520f168b4d1fe5
Jack <62153707+JackCo100@users.noreply.github.com>
2026-02-05 11:54:00 +0000

    Character limit on input fields

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -6,10 +6,10 @@ export default function NavBar({ handleSubmit, handleStnCodeChange, handleViaCod
       <p>Enter a station code and find live arrivals and departures.</p>
       <form action={handleSubmit}>
         <label htmlFor="stationCode">Station Code: </label>
-        <input type="text" id="stationCode" name="stationCode" onChange={handleStnCodeChange} value={stationCode} placeholder="Station code" className='border rounded px-2 m-2' required />
+        <input type="text" id="stationCode" name="stationCode" onChange={handleStnCodeChange} value={stationCode} placeholder="Station code" className='border rounded px-2 m-2' required maxLength={3}/>
         
         <label htmlFor="viaCode"> Calling at (optional): </label>
-        <input type="text" id="viaCode" name="viaCode" onChange={handleViaCodeChange} value={viaCode} placeholder='calling at' className='border rounded px-2 m-2'/>
+        <input type="text" id="viaCode" name="viaCode" onChange={handleViaCodeChange} value={viaCode} placeholder='calling at' className='border rounded px-2 m-2' maxLength={3}/>
         <button type="submit" className="bg-blue-300 hover:bg-blue-500 cursor-pointer px-1 rounded">Get Times</button>
       </form>
     </div>
